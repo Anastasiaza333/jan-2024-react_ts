2b9bae473fa6b37eb0ab3fed65e740588150331b
Anastasiaza333 <anastasiaza05@gmail.com>
2024-05-19 22:55:16 +0300

    commit for part 1

---
+++ src/components/charachter/Chrachter.tsx
@@ -0,0 +1,15 @@
+import React , {FC} from "react";
+
+interface IProps{
+    name:string;
+    image:string;
+}
+const Chrachter: FC <IProps> = (xxx) => {
+    return (
+        <div>
+            {/*<h2>hello</h2>*/}
+            {/*<img src={'https://stylecaster.com/wp-content/uploads/2012/02/157728_13288080041.jpg'}/>*/}
+        </div>
+    );
+};
+export default Chrachter;
