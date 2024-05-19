bc561a43ed06ac0198b72c03de83459e28faf18f
Anastasiaza333 <anastasiaza05@gmail.com>
2024-05-19 22:55:02 +0300

    commit for part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, {FC} from 'react';
 import './App.css';
+import Chrachter from "./components/charachter/Chrachter";
 
 const App:FC=()=>{
   return(
       <>
-          <h2>hello</h2>
+         <Chrachter name = {'bart'}
+                    image = {'https://freepngimg.com/thumb/bart_simpson/8-2-bart-simpson-transparent.png'}/>
       </>
+
   );
 }
 export default App;
